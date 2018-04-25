438db2bd65a7ce67ebbe0288e8780990c75c1a31
Jesse Schwartzentruber <truber@mozilla.com>
2018-04-24 17:51:43 -0700

    Add docker compose

--- setup.sh
+++ setup.sh
@@ -15,6 +15,12 @@ mkdir -p /root/.docker
 docker run --rm mozillasecurity/credstash get docker-login-config-json > /root/.docker/config.json
 chmod 0600 /root/.docker/config.json
 
+# Install docker compose
+sudo mkdir -p /opt/bin
+LATEST_VERSION="$(curl -Ls 'https://api.github.com/repos/docker/compose/releases/latest' | awk -f JSON.awk - | awk '/^\["tag_name"\]/ { gsub(/"/,""); print $2 }')"
+sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /opt/bin/docker-compose
+sudo chmod +x /opt/bin/docker-compose
+
 # Fix permissions
 chown core:core -R /home/core
 
