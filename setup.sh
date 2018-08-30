1b5f2dd90f65ca7f7d8fcaad82707aafe4e9a4c8
Jesse Schwartzentruber <truber@mozilla.com>
2018-08-30 13:52:14 -0400

    Add support for installing and running bootstrap on ubuntu as well as coreos

--- setup.sh
+++ setup.sh
@@ -1,28 +1,37 @@
 #!/bin/sh -exu
+if grep -q Ubuntu /etc/issue
+then
+  apt-get update
+  apt-get install -y docker.io
+  adduser ubuntu docker
 
-cd /home/core/bootstrap
+  USER=ubuntu
+else
+  USER=core
+fi
 
-# Add extra authorized keys
-mkdir -p /home/core/.ssh
-cat .ssh/authorized_keys >> /home/core/.ssh/authorized_keys
+cd /home/$USER/bootstrap
 
 # Create login script
-cp login_to_docker.sh /home/core
+cp login_to_docker.sh /home/$USER
 
 # Get credstash credentials
 docker pull mozillasecurity/credstash:latest
 mkdir -p /root/.docker
 docker run --rm mozillasecurity/credstash get docker-login-config-json > /root/.docker/config.json
 chmod 0600 /root/.docker/config.json
+# Add extra authorized keys
+mkdir -p /home/$USER/.ssh
+docker run --rm mozillasecurity/credstash get grizzly-ssh-authorized-keys >> /home/$USER/.ssh/authorized_keys
 
 # Install docker compose
-sudo mkdir -p /opt/bin
+mkdir -p /opt/bin
 LATEST_VERSION="$(curl -Ls 'https://api.github.com/repos/docker/compose/releases/latest' | awk -f JSON.awk - | awk '/^\["tag_name"\]/ { gsub(/"/,""); print $2 }')"
-sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /opt/bin/docker-compose
-sudo chmod +x /opt/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /opt/bin/docker-compose
+chmod +x /opt/bin/docker-compose
 
 # Fix permissions
-chown core:core -R /home/core
+chown $USER:$USER -R /home/$USER
 
 # sysctls for AFL
 sysctl kernel.core_pattern=core
