48c38fed3185f87ad3db759aacb37b626a8b8c1b
Jesse Schwartzentruber <truber@mozilla.com>
2019-01-09 16:20:21 -0500

    Fix login script to always specify the docker username, and support root login easily.

--- login_to_docker.sh
+++ login_to_docker.sh
@@ -1,2 +1,6 @@
 #!/bin/sh -ex
-sudo docker exec -e "COLUMNS=$(tput cols)" -e "LINES=$(tput lines)" -it "$(sudo docker ps | grep grizzly | tail -n1 | cut -f1 -d' ')" "$@" /bin/bash
+if [ $(id -u) -eq 0 ]; then
+  docker exec -u root -e "COLUMNS=$(tput cols)" -e "LINES=$(tput lines)" -it "$(sudo docker ps | grep grizzly | tail -n1 | cut -f1 -d' ')" "$@" /bin/bash
+else
+  sudo docker exec -u worker -e "COLUMNS=$(tput cols)" -e "LINES=$(tput lines)" -it "$(sudo docker ps | grep grizzly | tail -n1 | cut -f1 -d' ')" "$@" /bin/bash
+fi
